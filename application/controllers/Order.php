9d2b10ed61b706cca69091869f89ae51bc17622f
Derrick <drick89@gmail.com>
2016-02-16 17:20:21 -0800

    step 6 working

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,7 +17,14 @@ class Order extends Application {
 
     // start a new order
     function neworder() {
-        //FIXME
+        $order_num = $this->orders->highest() + 1;
+
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        $this->orders->add($neworder);
 
         redirect('/order/display_menu/' . $order_num);
     }
@@ -29,7 +36,8 @@ class Order extends Application {
 
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        $this->data['title'] = "Order # " . $order_num
+            . ' (' . number_format($this->orders->total($order_num), 2) . ')';
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -63,13 +71,13 @@ class Order extends Application {
     
     // make a menu ordering column
     function make_column($category) {
-        //FIXME
+        return $this->menu->some('category', $category);
         return $items;
     }
 
     // add an item to an order
     function add($order_num, $item) {
-        //FIXME
+        $this->orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -78,7 +86,18 @@ class Order extends Application {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
-        //FIXME
+
+        $this->data['total'] = number_format($this->orders->total($order_num), 2);
+
+        $items = $this->orderitems->group($order_num);
+        foreach ($items as $item)
+        {
+            $menuitem = $this->menu->get($item->item);
+            $item->code = $menuitem->name;
+        }
+
+        $this->data['items'] = $items;
+        $this->data['okornot'] = $this->orders->validate($order_num)  ? "" : "disabled";
 
         $this->render();
     }
@@ -91,7 +110,7 @@ class Order extends Application {
 
     // cancel the order
     function cancel($order_num) {
-        //FIXME
+
         redirect('/');
     }
 
